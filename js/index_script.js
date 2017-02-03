4b111a3757ca09210cf701973fef023613ca5375
hannahrfreedman <hannah.r.freedman@gmail.com>
2017-02-03 15:24:44 -0500

    added formatting and began photo interactive section

--- js/index_script.js
+++ js/index_script.js
@@ -2,3 +2,50 @@
 $.scrollify({
     section: ".scroll"
 });
+
+$(document).ready(function(){
+    $("img.nicole").hover(function(){
+        $("img.parker").css("opacity", "0.3");
+        $("img.patrick").css("opacity", "0.3");
+		$("img.lupita").css("opacity", "0.3");
+		$("p.nicole").css("display", "block");
+        }, function(){
+        $("img.parker").css("opacity", "1");
+        $("img.patrick").css("opacity", "1");
+        $("img.lupita").css("opacity", "1");
+        $("p.nicole").css("display", "none");
+    });
+    $("img.parker").hover(function(){
+        $("img.nicole").css("opacity", "0.3");
+        $("img.patrick").css("opacity", "0.3");
+		$("img.lupita").css("opacity", "0.3");
+		$("p.parker").css("display", "block");
+        }, function(){
+        $("img.nicole").css("opacity", "1");
+        $("img.patrick").css("opacity", "1");
+        $("img.lupita").css("opacity", "1");
+        $("p.parker").css("display", "none");
+    });
+	$("img.patrick").hover(function(){
+        $("img.nicole").css("opacity", "0.3");
+        $("img.parker").css("opacity", "0.3");
+		$("img.lupita").css("opacity", "0.3");
+		$("p.patrick").css("display", "block");
+        }, function(){
+        $("img.nicole").css("opacity", "1");
+        $("img.parker").css("opacity", "1");
+        $("img.lupita").css("opacity", "1");
+        $("p.patrick").css("display", "none");
+    });
+	$("img.lupita").hover(function(){
+        $("img.parker").css("opacity", "0.3");
+        $("img.patrick").css("opacity", "0.3");
+		$("img.nicole").css("opacity", "0.3");
+		$("p.lupita").css("display", "block");
+        }, function(){
+        $("img.parker").css("opacity", "1");
+        $("img.patrick").css("opacity", "1");
+        $("img.nicole").css("opacity", "1");
+        $("p.lupita").css("display", "none");
+    });
+});
