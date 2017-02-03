44e47f621dc84370a2c66e594e32633fe567a0ad
Justin Sullivan <justinnsullivan@gmail.com>
2017-02-03 16:51:40 -0500

    adjusting Hannahs killer work

--- js/index_script.js
+++ js/index_script.js
@@ -4,48 +4,14 @@ $.scrollify({
 });
 
 $(document).ready(function(){
-    $("img.nicole").hover(function(){
-        $("img.parker").css("opacity", "0.3");
-        $("img.patrick").css("opacity", "0.3");
-		$("img.lupita").css("opacity", "0.3");
-		$("p.nicole").css("display", "block");
-        }, function(){
-        $("img.parker").css("opacity", "1");
-        $("img.patrick").css("opacity", "1");
-        $("img.lupita").css("opacity", "1");
-        $("p.nicole").css("display", "none");
-    });
-    $("img.parker").hover(function(){
-        $("img.nicole").css("opacity", "0.3");
-        $("img.patrick").css("opacity", "0.3");
-		$("img.lupita").css("opacity", "0.3");
-		$("p.parker").css("display", "block");
-        }, function(){
-        $("img.nicole").css("opacity", "1");
-        $("img.patrick").css("opacity", "1");
-        $("img.lupita").css("opacity", "1");
-        $("p.parker").css("display", "none");
-    });
-	$("img.patrick").hover(function(){
-        $("img.nicole").css("opacity", "0.3");
-        $("img.parker").css("opacity", "0.3");
-		$("img.lupita").css("opacity", "0.3");
-		$("p.patrick").css("display", "block");
-        }, function(){
-        $("img.nicole").css("opacity", "1");
-        $("img.parker").css("opacity", "1");
-        $("img.lupita").css("opacity", "1");
-        $("p.patrick").css("display", "none");
-    });
-	$("img.lupita").hover(function(){
-        $("img.parker").css("opacity", "0.3");
-        $("img.patrick").css("opacity", "0.3");
-		$("img.nicole").css("opacity", "0.3");
-		$("p.lupita").css("display", "block");
-        }, function(){
-        $("img.parker").css("opacity", "1");
-        $("img.patrick").css("opacity", "1");
-        $("img.nicole").css("opacity", "1");
-        $("p.lupita").css("display", "none");
-    });
+
+    $('.img').mouseover(function(){
+        var name = $(this).attr('class');
+        $('.img').removeClass('active');
+        $(this).addClass('active');
+        name = name.replace('img ', '');
+        var quote = $('.line.' + name);
+        $('.line.active').removeClass('active');
+        quote.addClass('active');
+    })
 });
